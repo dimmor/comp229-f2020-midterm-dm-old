1b008d7d657836986dc5f0f561bba11c9fa84b05
Dmitriy Morozov <rifename@gmail.com>
2020-10-30 13:16:43 -0400

    connection to the external database on Atlas MongoDB commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  
+  //Atlas MongoDB access 
+  "URI": "mongodb+srv://dimmor:9SqSZIW2MjzLrGYR@mongodbserver.xbu2n.mongodb.net/book_app?retryWrites=true&w=majority"
 };
